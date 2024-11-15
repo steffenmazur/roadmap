a75bfd40721e78087738809ecc9b1caecc7d2b2b
PRO-MICRON\steffen.mazur <steffen.mazur@pro-micron.de>
2024-11-15 18:29:41 +0100

    Initial commit for new project ROADMAP

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
